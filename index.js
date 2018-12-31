206e328d804ba165428dc58342c7282ed2ff2905
crazyRubix <muneebkt@gmail.com>
2018-12-31 20:31:08 +0530

    adding route

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const hapi = require('hapi');
 const server = hapi.server({
     port: 4000,
     host: 'localhost'
-})
+});
 
 const init = async() =>{
     await server.start();
     console.log(`server is running at :${server.info.uri}`)
-}
+    server.route({
+        method:"GET",
+        path:'/',
+        handler: function(request,reply){
+            return "<h1>Modern APi using Hapi </h1>"
+        }
+    });
+};
+
 init();
